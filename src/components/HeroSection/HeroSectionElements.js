a67c50b9298cc8eb2bdb64251c8934a4eb4b17b6
Farid Guzman <farid@woonmodule.nl>
2021-11-09 14:47:42 +0100

    hero box

--- src/components/HeroSection/HeroSectionElements.js
+++ src/components/HeroSection/HeroSectionElements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md'
 
 export const HeroContainer = styled.div`
 background: #6a6a77;
@@ -29,4 +30,64 @@ height: 100%;
 object-fit: cover;
 background: #6a6a77;
 
+`
+
+export const HeroContent = styled.div`
+z-index: 3;
+max-width: 1200px;
+position: absolute;
+padding: 8px 24px;
+display: flex;
+flex-direction: column;
+align-items: center;
+top: 30%;
+left: 20%;
+`
+
+export const HeroH1 = styled.h1`
+color: #fff;
+font-size: 48px;
+text-align: center;
+
+@media screen and (max-width: 768px) {
+    font-size: 40px;
+}
+
+@media screen and (max-width: 480px) {
+    font-size: 32px;
+}
+
+`
+
+export const HeroP = styled.p`
+margin-top: 24px;
+color: #fff;
+font-size: 24px;
+text-align: center;
+max-width: 600px;
+
+@media screen and (max-width: 768px) {
+    font-size: 24px;
+}
+
+@media screen and (max-width: 480px) {
+    font-size: 18px;
+}
+`
+
+export const HeroBtnWrapper = styled.div`
+margin-top: 32px;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const ArrowForward = styled(MdArrowForward)`
+margin-left: 8px;
+font-size: 20px;
+`
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+margin-left: 8px;
+font-size: 20px;
 `
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Video from '../../video/reactapp.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroSectionElements'
+import { Button } from '../ButtonElement'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroSectionElements'
 
 const HeroSection = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoplay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Map Tool</HeroH1>
+                <HeroP>Try our free account</HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}>
+                        Get started {hover ? <ArrowForward /> : <ArrowRight />}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
 
         </HeroContainer>
     )
