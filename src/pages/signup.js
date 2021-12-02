4242c7fa4d7b7f137bacf8fafa4d121256835b12
Jimmy Guzman <jimmy@creapolis.com>
2021-12-02 14:25:25 +0100

    signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,6 +2,10 @@ import React from 'react'
 import SignUp from '../components/SignUp'
 import NavBar from '../components/NavBar'
 import StepsBar from '../components/Steps'
+import NewProject from "../pages/newproject";
+import EditHouse from "../components/EditHouse";
+import NewProjectPage from '../components/NewProject';
+
 // import StepWizard from 'react-step-wizard'
 
 const SignUpPage = () => {
@@ -11,6 +15,9 @@ const SignUpPage = () => {
                 <NavBar />
                 <StepsBar/>
                 <SignUp />
+                <NewProjectPage />
+                <EditHouse />
+
             </div>
         </>
     )
