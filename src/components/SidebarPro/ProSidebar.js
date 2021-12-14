e363cd192072d03df6358419c8a663aac23748e5
Farid Guzman <farid@woonmodule.nl>
2021-12-14 17:18:02 +0100

    prosidebar main

--- src/components/SidebarPro/ProSidebar.js
+++ src/components/SidebarPro/ProSidebar.js
@@ -5,9 +5,13 @@ import Data from './Data';
 import 'sidebar.styles.scss'
 
 function ProSidebar() {
+
+    const [locale, setLocale] = useState('en');
     return (
-        <IntlProvider locale={locale} messages={messages[locale]}>
+        <IntlProvider locale={locale} Data={Data[locale]}>
             <Layout setLocale={setLocale} />
         </IntlProvider>
     )
 }
+
+export default ProSidebar
